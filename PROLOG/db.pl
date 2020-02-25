52da81b9ac96be7411356b875ad1506352706050
Guilherme Alves Carvalho <60170326+GULABoso@users.noreply.github.com>
2020-02-25 19:44:55 -0300

    Add files via upload

---
+++ PROLOG/db.pl
@@ -0,0 +1,48 @@
+%fatos
+loves(romeo, juliet).
+loves(juliet, romeo) :- loves(romeo, juliet).
+
+happy(albert).
+happy(alice).
+happy(bob).
+happy(bill).
+with_albert(alice).
+with_albert(bob).
+
+parent(albert,bob).
+parent(albert,betsy).
+parent(albert,bill).
+
+parent(alice,bob).
+parent(alice,betsy).
+parent(alice,bill).
+
+parent(bob,carl).
+parent(bob,charlie).
+
+runs(albert) :- 
+    happy(albert).
+
+dances(X) :-
+    happy(X),
+    with_albert(X).
+
+does_dance(X) :- 
+    dances(X),
+    format('~w dances when It\'s happy and with Albert',[X]).
+
+swims(X) :-
+    happy(X).
+
+is_even(X) :-
+    Y is X//2, Y =:= 0.
+
+what_grade(5) :-
+        write('You\'re gay!'),nl,write('Die!').
+
+related(X,Y) :-
+    parent(X,Y).
+
+related(X,Y) :-
+    parent(X,Z),
+    related(Z,Y).
