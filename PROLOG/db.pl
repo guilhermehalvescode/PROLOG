3b72d5cd55c225b73f53fef52e5ddf08518df364
Guilherme Alves Carvalho <60170326+GULABoso@users.noreply.github.com>
2020-02-26 20:17:26 -0300

    Atualizações

--- PROLOG/db.pl
+++ PROLOG/db.pl
@@ -2,6 +2,8 @@
 loves(romeo, juliet).
 loves(juliet, romeo) :- loves(romeo, juliet).
 
+:- dynamic(happy/2).
+
 happy(albert).
 happy(alice).
 happy(bob).
@@ -35,10 +37,10 @@ swims(X) :-
     happy(X).
 
 is_even(X) :-
-    Y is X//2, Y =:= 0.
+    Y is X//2, X =:= 2 * Y.
 
 what_grade(5) :-
-        write('You\'re pretty!'),nl,write('!').
+        write('You\'re gay!'),nl,write('Die!').
 
 related(X,Y) :-
     parent(X,Y).
@@ -46,3 +48,38 @@ related(X,Y) :-
 related(X,Y) :-
     parent(X,Z),
     related(Z,Y).
+
+show_name :-
+    write('What is your name?'),nl,
+    read(X),
+    format('Hi ~w! Welcome to prolog',[X]).
+
+write_to_file(File,Text):-
+    open(File, write, Stream),
+    write(Stream, Text),nl,
+    close(Stream).
+            
+read_file(File) :-
+    open(File, read, Stream),
+    get_char(Stream, Char1),
+    process_stream(Stream, Char1),
+    close(Stream).
+
+process_stream(_,end_of_file) :- !.
+
+process_stream(Stream, Char) :-
+    write(Char),
+    get_char(Stream,Char2),
+    process_stream(Stream, Char2).
+
+guess_num :- loop(start).
+
+loop(15) :- write('Acertou!!').
+
+loop(X) :-
+    \+ (X = 15),
+    write('Adivinhe o número '),
+    read(Guess),
+    write(Guess),
+    write(' is not the number'),nl,
+    loop(Guess).
